4394a7207709fe1e18fa8edc75448b8f3ac467e1
Nicole Cooper-White <nwhite1018@gmail.com>
2018-07-24 18:32:23 -0600

    add omdb

--- liri.js
+++ liri.js
@@ -58,3 +58,96 @@ switch(command){
   break;
 }
 
+function showTweets(){
+  //Display last 20 Tweets
+  var screenName = {screen_name: "NicoleAC"};
+  client.get("statuses/user_timeline", screenName, function(error, tweets, response){
+    if(error){
+      console.log("Error occurred");
+    }else{
+      for (var i = 0; i<tweets.length; i++){
+        var date = tweets[i].created_at;
+        console.log("@NicoleAC: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        console.log("----------------------------")
+
+        //adds text to log.txt file
+        fs.appendFile("log.txt", "@NicoleAC: " + tweets[i].text + " Created At: " + date.substring(0, 19));
+        fs.appendFile("log.txt", "----------------------------");
+      }
+    }
+  });
+}
+
+function spotifySong(song){
+  spotify.search({ type: "track", query: song}, function(error, data){
+    if(error){
+      console.log("error occurred");
+    } else {
+      for(var i = 0; i<data.tracks.items.length; i++){
+        var songData = data.tracks.items[i];
+        //artist
+        console.log("Artist: " + songData.artists[0].name);
+
+        //song name
+        console.log("Song: " + songData.name);
+
+        //spotify preview link
+        console.log("Preview URL: " + songData.preview_url);
+
+        //album name
+        console.log("Album: " + songData.preview.name);
+        console.log("---------------------------")
+
+        //adds text to log.txt
+        fs.appendFile("log.txt", songData.artists[0].name);
+        fs.appendFile("log.txt", songData.name);
+        fs.appendFile("log.txt", songData.preview_url);
+        fs.appendFile("log.txt", songData.album.name);
+        console.log("---------------------------")
+      }
+    }
+  })
+}
+
+function omdbData(movie){
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+
+  request(queryUrl, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      var body = JSON.parse(body);
+
+      console.log("Title: " + body.Title);
+      console.log("Release Year: " + body.Year);
+      console.log("IMdB Rating: " + body.imdbRating);
+      console.log("Country: " + body.Country);
+      console.log("Language: " + body.Language);
+      console.log("Plot: " + body.Plot);
+      console.log("Actors: " + body.Actors);
+      console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+      console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+      //adds text to the log.txt
+      fs.appendFile("log.txt", "Title: " + body.Title);
+      fs.appendFile("log.txt", "Release Year: " + body.Year);
+      fs.appendFile("log.txt", "IMdB Rating: " + body.imdbRating);
+      fs.appendFile("log.txt", "Country: " + body.Country);
+      fs.appendFile("log.txt", "Language: " + body.Language);
+      fs.appendFile("log.txt", "Plot: " + body.Plot);
+      fs.appendFile("log.txt", "Actors: " + body.Actors);
+      fs.appendFile("log.txt", "Rotten Tomatoes Rating: " + body.tomatoRating);
+      fs.appendFile("log.txt", "Rotten Tomatoes URL: " + body.tomatoURL);
+    }
+    if(movie === "Mr. Nobody"){
+      console.log("--------------------------");
+      console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+      console.log("It's on Netflix!");
+
+      //adds text to log.txt
+      fs.appendFile('log.txt', "------------------------");
+      fs.appendFile('log.txt', "If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+
+      fs.appendFile("log.txt", "It's on Netflix!");
+
+    }
+  });
+}
